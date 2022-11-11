d8ef9e1092d429fea8d72b899df735f03e1120b4
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2022-11-11 23:05:38 +0900

    chore: match linter indentation styles

--- src/runtime/multiplayer/index.js
+++ src/runtime/multiplayer/index.js
@@ -1,111 +1,120 @@
+/* eslint-disable no-console */
 import event from '../event';
-import { set } from '../state';
-import { Peer } from 'peerjs';
+import {set} from '../state';
+import {Peer} from 'peerjs';
 import logger from '../logger';
 
 const {log} = logger('multiplayer');
 
-export function init () {
-    document.addEventListener('DOMContentLoaded', () => {
-        sessionData.id = '';
-        document.getElementById('host-game').addEventListener('click', () => {
-            sessionData.id = '';
-            hostGame();
-        });
-        document.getElementById('join-game').addEventListener('click', () => {
-            const sid = sessionData.id;
-            if (sid) {
-                joinGame(sid);
-            } else {
-                log('Failed to join session due to missing session id');
-                setConnectionStatus('Error');
-            }
-        });
-    })
-};
+export function init() {
+	document.addEventListener('DOMContentLoaded', () => {
+		sessionData.id = '';
+		document.getElementById('host-game').addEventListener('click', () => {
+			sessionData.id = '';
+			hostGame();
+		});
+		document.getElementById('join-game').addEventListener('click', () => {
+			const sid = sessionData.id;
+
+			if (sid) {
+				joinGame(sid);
+			} else {
+				log('Failed to join session due to missing session id');
+				setConnectionStatus('Error');
+			}
+		});
+	});
+}
 
 const sessionData = {
-    get id () {
-        return document.getElementById('session-id') && document.getElementById('session-id').value;
-    },
-    set id (value) {
-        document.getElementById('session-id').value = value;
-    }
+	get id() {
+		return (
+			document.getElementById('session-id') &&
+			document.getElementById('session-id').value
+		);
+	},
+	set id(value) {
+		document.getElementById('session-id').value = value;
+	}
 };
 
-function setConnectionStatus (status) {
-    document.getElementById('connection-status').innerHTML = status;
+function setConnectionStatus(status) {
+	document.getElementById('connection-status').innerHTML = status;
 }
 
-function hostGame () {
-    setConnectionStatus('Connecting...');
-    const peer = new Peer();
-    peer.on('error', (err) => {
-        log('peer connection error: host');
-        console.error(err);
-        setConnectionStatus('Error');
-    });
-    peer.on('open', (id) => {
-        sessionData.id = id;
-        setConnectionStatus('Connected');
-    });
-    peer.on('connection', (conn) => {
-        setupConnection(conn);
-    });
+function hostGame() {
+	setConnectionStatus('Connecting...');
+	const peer = new Peer();
+
+	peer.on('error', err => {
+		log('peer connection error: host');
+		console.error(err);
+		setConnectionStatus('Error');
+	});
+	peer.on('open', id => {
+		sessionData.id = id;
+		setConnectionStatus('Connected');
+	});
+	peer.on('connection', conn => {
+		setupConnection(conn);
+	});
 }
 
-function joinGame (sessionID) {
-    setConnectionStatus('Connecting...');
-    const peer = new Peer();
-    peer.on('error', (err) => {
-        log('peer connection error: join');
-        console.error(err);
-        setConnectionStatus('Error');
-    });
-    peer.on('open', (id) => {
-        setupConnection(peer.connect(sessionID));
-    });
+function joinGame(sessionID) {
+	setConnectionStatus('Connecting...');
+	const peer = new Peer();
+
+	peer.on('error', err => {
+		log('peer connection error: join');
+		console.error(err);
+		setConnectionStatus('Error');
+	});
+	peer.on('open', id => {
+		setupConnection(peer.connect(sessionID));
+	});
 }
 
-function setupConnection (connection) {
-    // Receive messages
-    connection.on('data', (data) => {
-        if (shouldSyncState(data.name)) {
-            log('Syncing FROM another client...');
-            log(`name: ${data.name}, previous: ${data.previous}, value: ${data.value}`);
-            set(data.name, data.value, true);
-        }
-    });
+function setupConnection(connection) {
+	// Receive messages
+	connection.on('data', data => {
+		if (shouldSyncState(data.name)) {
+			log('Syncing FROM another client...');
+			log(
+				`name: ${data.name}, previous: ${data.previous}, value: ${data.value}`
+			);
+			set(data.name, data.value, true);
+		}
+	});
 
-    connection.on('open', () => {
-        setConnectionStatus('Connected');
+	connection.on('open', () => {
+		setConnectionStatus('Connected');
 
-        // Send messages
-        event.on('state-change', ({name, previous, value, isFromPeers}) => {
-            if (!isFromPeers && shouldSyncState(name)) {
-                log('Syncing TO other clients...');
-                log(`name: ${name}, previous: ${previous}, value: ${value}`);
-                connection.send({name, previous, value});
-            }
-        });
-    });
+		// Send messages
+		event.on('state-change', ({name, previous, value, isFromPeers}) => {
+			if (!isFromPeers && shouldSyncState(name)) {
+				log('Syncing TO other clients...');
+				log(`name: ${name}, previous: ${previous}, value: ${value}`);
+				connection.send({name, previous, value});
+			}
+		});
+	});
 
-    connection.on('error', (err) => {
-        log('peer connection error: connection issue');
-        console.error(err);
-        setConnectionStatus('Error');
-    });
+	connection.on('error', err => {
+		log('peer connection error: connection issue');
+		console.error(err);
+		setConnectionStatus('Error');
+	});
 }
 
-function shouldSyncState (name) {
-    /**
-     * Syncing rules
-     * 1. If a variable should be synced across players, start the variable with '$'
-     * 
-     * For example, if you store a key in a drawer and you want only one player to be able to pick it up,
-     * you'll have two variables: hasDrawerKey and $isDrawerKeyTaken. You'll set up a condition that the
-     * drawer key cannot be retrieved if $isDrawerKeyTaken is true, which gets set across all players when
-     * a player picks up the key (an event where hasDrawerKey and $isDrawerKeyTaken are both set to true)
-     */
-    return name.charAt(0) === '$';
+function shouldSyncState(name) {
+	/**
+	 * Syncing rules
+	 * 1. If a variable should be synced across players, start the variable with '$'
+	 *
+	 * For example, if you store a key in a drawer and you want only one player to be able to pick it up,
+	 * you'll have two variables: hasDrawerKey and $isDrawerKeyTaken. You'll set up a condition that the
+	 * drawer key cannot be retrieved if $isDrawerKeyTaken is true, which gets set across all players when
+	 * a player picks up the key (an event where hasDrawerKey and $isDrawerKeyTaken are both set to true)
+	 */
+	return name.charAt(0) === '$';
 }
